e20b2368257b5f68d736f1452e15d6fdd027611e
dedmamn <dimas234325325@gmail.com>
2024-03-15 21:10:22 +0300

    Func: Add reg state

--- src/states/states.py
+++ src/states/states.py
@@ -1,4 +1,16 @@
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
+
 # Create your states here.
+
+class RegistrationStateGroup(StatesGroup):
+    waiting_for_role = State()
+    waiting_for_password = State()
+
+
+class ProfileSpecialist(StatesGroup):
+    waiting_for_fio = State()
+    waiting_for_grade = State()
+    waiting_for_skills = State()
+
